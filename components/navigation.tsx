8c16811361b1f539b8f5bc9a071a1c5bee3417b2
dazz6 <dazz6@kakao.com>
2025-03-19 16:22:06 +0900

    2.5 'use client'

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,4 +1,9 @@
 "use client";
+// 정적 HTML 파일 로드 후 동적 요소 로드 과정은 이 문구가 기재되어 있어야만 발생함
+// 해당 components가 client에서 interactive 해야 한다는 선언
+// BackEnd에서 Render 되고 FrontEnd에서 hydrate 된다는 것 (Not Only CSR)
+// 이 문구가 없을 경우 Server Component 가 됨
+// 브라우저가 선택적으로 JS 로드하므로 효율적 (속도 및 성능 측면에서 좋음)
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -26,10 +31,3 @@ export default function Navigation() {
     </nav>
   );
 }
-
-// about-us    ->    Boring HTML    ->        :)    ->            init(Boring HTML)
-// 접근               backend에서 생성한 html     JavaScript 로드      React Component로 작동
-
-// hydration은 단순 HTML을 React Component로 변환하는 과정!!
-// 초기에 사용자가 페이지에 접근할 때 일단은 정적 HTML 출력,
-// 그동안 BackEnd에서 next.js를 로딩하고 프레임워크를 초기화하여 React 화함
