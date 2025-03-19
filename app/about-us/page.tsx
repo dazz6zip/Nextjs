c6b80647799f35ce2ca696f19de3aa70d42e941c
dazz6 <dazz6@kakao.com>
2025-03-19 15:31:31 +0900

    2.1 Defining Routes

---
+++ app/about-us/page.tsx
@@ -0,0 +1,4 @@
+export default function AboutUs() {
+  // 디렉토리 생성 후 page 파일 생성
+  return <h1>About us!</h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,3 +3,8 @@ export default function Page() {
   // 파일은 무조건 app/page
   return <h1>Hello NextJs</h1>;
 }
+
+/*
+기존 React 상에서의 Route는 특정 url 접근시 특정 Component 렌더링
+Next.js는 파일 시스템을 통해서 url을 표현
+*/
