ece39188d020cf10488994d722334688c1811853
dazz6 <dazz6@kakao.com>
2025-03-20 20:01:47 +0900

    3.4 Parallel Requests

--- app/(home)/loading.tsx
+++ app/(home)/loading.tsx
@@ -1,5 +1,3 @@
 export default function Loading() {
-  // fetch 로딩 중 자동으로 출력됨
-  // 로딩 종료시 자동으로 page 출력
   return <h1>Loading...</h1>;
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,5 +1,30 @@
-export default async function MovieDetail({ params, searchParams }) {
-  const { id } = await params;
+import { API_URL } from "../../../(home)/page";
 
-  return <h1>Movie {id}</h1>;
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  console.log(`start fetching`);
+  // Promise.all -> 동시에 await
+  const [movie, video] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log(`end fetching`);
+  // 기존 코드는 순차적으로 실행됨 (movie 이후 video 로드)
+  // Promise.all 사용으로 병렬 실행 처리
+
+  return <h1>{movie.title}</h1>;
 }
