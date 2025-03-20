dbea0bb19086e98ef94bc942f3e900ee36abe8e2
dazz6 <dazz6@kakao.com>
2025-03-20 21:26:33 +0900

    4.4 Dynamic Metadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default async function MovieDetail({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+// 동적인 제목을 가지고 있는 페이지를 위해 존재
+export async function generateMetadata({ params: { id } }: IParams) {
+  // 캐시된 데이터를 사용하므로 api 재호출이 아님
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
