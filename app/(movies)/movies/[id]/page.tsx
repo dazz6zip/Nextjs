5a40c2dcffc7efbf17f79eb9ee99d92e1583fc84
dazz6 <dazz6@kakao.com>
2025-03-20 22:45:38 +0900

    edit movie page

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,20 +2,28 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-  params: { id: string };
-}
+type IParams = {
+  id: string;
+};
 
-// 동적인 제목을 가지고 있는 페이지를 위해 존재
-export async function generateMetadata({ params: { id } }: IParams) {
-  // 캐시된 데이터를 사용하므로 api 재호출이 아님
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<IParams>;
+}) {
+  const { id } = await params;
   const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetail({ params: { id } }: IParams) {
+export default async function MovieDetail({
+  params,
+}: {
+  params: Promise<IParams>;
+}) {
+  const { id } = await params;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
